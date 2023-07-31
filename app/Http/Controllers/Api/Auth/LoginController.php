ce642e3503d742a8e931c134efe4d7c70aec3053
Okumura0903 <127096042+Okumura0903@users.noreply.github.com>
2023-07-31 22:11:01 +0900

    initial

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        $user=User::where('email',$request->email)->first();
+        if(!$user || !Hash::check($request->password,$user->password)){
+            throw ValidationException::withMessages([
+                'email'=>['パスワードが違います！']
+            ]);
+        }
+        return response()->json([
+            'user'=>$user,
+            'token'=>$user->createToken('Laravel_api_token')->plainTextToken
+       ]);
+    }
+}
